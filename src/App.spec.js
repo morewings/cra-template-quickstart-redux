b41ae43347fa654ba98fc399225fb47796383b9c
Dima Vyshniakov <morewings@gmail.com>
2020-01-02 08:00:31 +0100

    WIP

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import toJson from 'enzyme-to-json';
 import {Provider} from 'react-redux';
 import store from 'store';
 import App from 'App';
@@ -11,5 +10,5 @@ it('renders without crashing', () => {
       <App />
     </Provider>
   );
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
