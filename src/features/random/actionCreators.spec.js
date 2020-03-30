ce61c60cd77a92d619cc203a9d977c11958a638c
Dima Vyshniakov <1158959+morewings@users.noreply.github.com>
2020-03-30 20:26:40 +0200

    Update actionCreators.spec.js (#20)
    * Update actionCreators.spec.js
    
    * Update actionCreators.spec.js
    
    * Update actionCreators.spec.js
    
    * Update actionCreators.spec.js

--- src/features/random/actionCreators.spec.js
+++ src/features/random/actionCreators.spec.js
@@ -100,6 +100,7 @@ describe('features > counter > useActions', () => {
     it.each([[mockNetworkError], [mock404], [mockTimeout]])(
       `it handles API fetching errors`,
       async mockResponse => {
+        let hasThrown;
         const {result} = renderHook(() => useActions(), {
           wrapper: ({children}) => (
             <Provider store={store}>{children}</Provider>
@@ -113,7 +114,9 @@ describe('features > counter > useActions', () => {
          */
         try {
           await result.current.getNumber();
-        } catch (e) {
+        } catch {
+          hasThrown = true;
+        } finally {
           expect(store.getActions()[0]).toEqual({
             type: `${GET_RANDOM_NUMBER}_PENDING`,
           });
@@ -122,6 +125,7 @@ describe('features > counter > useActions', () => {
           );
           expect(store.getActions()[1].payload).toBeInstanceOf(Error);
           expect(store.getActions()[1].payload).toMatchSnapshot();
+          expect(hasThrown).toBe(true);
         }
       }
     );
