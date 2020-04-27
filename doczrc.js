d7ee42fbb198d41f5ae28326f614a12b1aec95f5
Dima Vyshniakov <morewings@gmail.com>
2020-04-27 14:16:10 +0200

    WIP

---
+++ doczrc.js
@@ -0,0 +1,14 @@
+export default {
+  files: '**/*.mdx',
+  title: 'Quickstart Redux Create React App template',
+  menu: [
+    'Intro',
+    'Install',
+    'Redux',
+    'Code quality tools',
+    'Testing',
+    'Style options',
+    'Absolute imports',
+  ],
+  dest: '/docz-dist',
+};
