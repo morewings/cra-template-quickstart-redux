a1b072cc521923c3e392df8919186151c0f8d062
Dima Vyshniakov <1158959+morewings@users.noreply.github.com>
2021-08-29 17:40:37 +0200

    add test action (#251)
    * add test action
    
    * bump node
    
    * fix errors

--- src/withProvider.js
+++ src/withProvider.js
@@ -4,10 +4,14 @@ import React from 'react';
  * Utility Higher Order Component factory. Returns HOC which takes another
  * Component and wraps it with given Provider.
  */
-const withProvider = ({store, Provider}) => WrappedComponent => props => (
-  <Provider store={store}>
-    <WrappedComponent {...props} />
-  </Provider>
-);
+const withProvider =
+  ({store, Provider}) =>
+  WrappedComponent =>
+  props =>
+    (
+      <Provider store={store}>
+        <WrappedComponent {...props} />
+      </Provider>
+    );
 
 export default withProvider;
