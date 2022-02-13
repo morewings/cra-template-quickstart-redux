0bbadb9d1e4b313e7cbdfbc69d57d21f5fbe4704
Dima Vyshniakov <morewings@gmail.com>
2022-02-13 21:30:07 +0100

    update storybook and node version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,10 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../docs/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../docs/**/*.stories.mdx"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/addon-essentials"
   ]
 }
